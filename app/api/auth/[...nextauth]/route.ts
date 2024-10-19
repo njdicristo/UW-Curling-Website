979a1f203884ca27d118281e35062a1c89d2170e
GeWangzi <56490458+GeWangzi@users.noreply.github.com>
2024-10-19 17:58:48 -0500

    fixed error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import { NextRequest, NextResponse } from 'next/server';
 
 export const authOptions = {
   providers: [
@@ -14,14 +13,4 @@ export const authOptions = {
 
 const handler = NextAuth(authOptions);
 
-
-export async function GET(req: NextRequest) {
-  const response = await handler(req, NextResponse);
-  return response;
-}
-
-export async function POST(req: NextRequest) {
-  // Call the NextAuth handler for the POST request
-  const response = await handler(req, NextResponse);
-  return response;
-}
+export { handler as GET, handler as POST };
