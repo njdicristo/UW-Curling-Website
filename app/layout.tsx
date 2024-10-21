800d7507e30452c22626c2e0e97340fe5fd458e4
njdicristo <nicholasjdicr@gmail.com>
2024-10-21 15:33:04 -0500

    Added MaterialUI for Nextjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
 import { NextAuthProvider } from '@/components/nextauth-proivder';
 import { ReactNode } from 'react';
-
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
         {/* Wrapping the entire app with the SessionProvider */}
+        <AppRouterCacheProvider>
         <NextAuthProvider>
           {children}
         </NextAuthProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
