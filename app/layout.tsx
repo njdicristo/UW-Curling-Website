201e8dcf3872975a6b1e83135c1f351d6f9a064f
GeWangzi <56490458+GeWangzi@users.noreply.github.com>
2024-10-30 13:34:04 -0500

    cleaned code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,30 @@
-import { NextAuthProvider } from '@/components/nextauth-proivder';
 import { ReactNode } from 'react';
+import { getServerSession } from 'next-auth';
+import NavMenu from '@/components/NavMenu';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { Roboto } from 'next/font/google';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../app/theme';
+import SessionProvider from '@/components/SessionProvider';
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-roboto',
 });
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default async function RootLayout({ children }: { children: ReactNode }) {
+  const session = await getServerSession();
   return (
     <html lang="en">
       <body className={roboto.variable}>
-        {/* Wrapping the entire app with the SessionProvider */}
-        <AppRouterCacheProvider>
-        <ThemeProvider theme={theme}>
-        <NextAuthProvider>
-          {children}
-        </NextAuthProvider>
-        </ThemeProvider>
-        </AppRouterCacheProvider>
+      <SessionProvider>
+          <AppRouterCacheProvider>
+            <ThemeProvider theme={theme}>
+              <NavMenu />
+              {children}
+            </ThemeProvider>
+          </AppRouterCacheProvider>
+        </SessionProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,7 @@
-"use client";
-
-import { useSession, signIn, signOut } from 'next-auth/react';
-import LoginButton from '@/components/login-btn';
-import { redirect } from 'next/dist/server/api-utils';
-import { useRef } from 'react';
-import { Link } from '@mui/material';
-
-export default function HomePage() {
-  const { data: session, status } = useSession();
-
-  if (status === 'loading') {
-    return <p>Loading...</p>;
-  }
-
-  if (!session) {
-    return (
-      <div>
-        <h1>Welcome to the Home Page</h1>
-        <p>You are not signed in.</p>
-        <button onClick={() => signIn()}>Sign in</button>
-        <Link href='/dashboard'><button>Dashboard</button></Link>
-        </div>
-    );
-  }
-
+export default function Home() {
   return (
     <div>
-      <h1>Welcome back, {session.user?.name}!</h1>
-      <p>Email: {session.user?.email}</p>
-      <button onClick={() => signOut()}>Sign out</button>
-      <Link href='/dashboard'><button>Dashboard</button></Link>
+      <h1>Home</h1>
     </div>
   );
 }
