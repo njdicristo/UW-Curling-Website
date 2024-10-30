baa5f036d8d44ab2f1947bc2f833fa8d743c5ca6
GeWangzi <56490458+GeWangzi@users.noreply.github.com>
2024-10-30 13:11:54 -0500

    minor tweaks

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,18 +4,8 @@ import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google';
 export const authOptions = {
   providers: [
     GoogleProvider({
-      profile(profile: GoogleProfile){
-        return {
-          ...profile,
-          role: profile.role ?? "member",
-          id: profile.at_hash,
-          email: profile.email,
-          image: profile.picture,
-          name: profile.name,
-        }
-      },
-      clientId: process.env.GOOGLE_ID!,
-      clientSecret: process.env.GOOGLE_SECRET!,
+      clientId: process.env.GOOGLE_ID! ?? "",
+      clientSecret: process.env.GOOGLE_SECRET! ?? "",
     }),
   ],
   callbacks: {
@@ -36,6 +26,6 @@ export const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 };
 
-const handler = NextAuth(authOptions);
+export const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
